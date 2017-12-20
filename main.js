4490b27992593e13e429776df1013a0e26417875
Marton Demeter <marton1029@gmail.com>
2017-12-19 23:37:02 -0800

    added error handling

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const rp = require('request-promise');
 const cheerio = require('cheerio');
-const writeFile = require('fs').writeFile;
-const execSync = require('child_process').execSync;
+const child_process = require('child_process');
+const fs = require('fs');
 const moment = require('moment');
-const secrets = require('./secrets.json');
+const rp = require('request-promise');
+const secrets = require('./secrets.js');
 
-var ts = Math.floor(new Date()/1000);
+var ts = moment().unix();
 
-execSync(`mkdir csci571_${ts} &&
+child_process.execSync(`mkdir csci571_${ts} &&
           mkdir csci571_${ts}/slides &&
           mkdir csci571_${ts}/exams`);
 
 process.on('SIGINT', () => process.exit(1));
 process.on('exit', (code) => {
-  if(code) execSync(`rm -rf csci571_${ts}`);
+  if(code) child_process.execSync(`rm -rf csci571_${ts}`);
 });
 
-rp('http://cs-server.usc.edu:45678/lectures.html')
-.then((data) => {
+rp('http://cs-server.usc.edu:45678/lectures.html').then((data) => {
   $ = cheerio.load(data);
   $('td').find('a').each((i, el) => {
     var re = new RegExp('.+?\.pdf');
@@ -37,11 +36,15 @@ rp('http://cs-server.usc.edu:45678/lectures.html')
       moment.suppressDeprecationWarnings = true;
       var date = moment(`${$(el).parent().parent().children().first().next().html()} 2017`).format('YY-MM-DD_');
       if(date === 'Invalid date') date = String();
-      rp(options)
-      .then((data) => {
-        console.log(`Finished ${cat}/${name}`);
-        writeFile(`csci571_${ts}/${cat}/${date}${name}`, data, 'binary', () => {});
-      });
+      rp(options).then((data) => {
+        console.log(`Downloaded ${cat}/${name}`);
+        fs.writeFile(`csci571_${ts}/${cat}/${date}${name}`, data, 'binary', () => {});
+      }).catch((error) => {
+        console.error(`Couldn't download ${cat}/${name}`);
+      })
     }
   });
-})
+}).catch((error) => {
+  console.error(`Couldn't retrieve lectures`);
+  process.exit(1);
+});
